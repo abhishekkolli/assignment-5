f2586e1ebee67d02b6a1da7b0bc4372fe9a7dcf3
abhishekkolli <93168947+abhishekkolli@users.noreply.github.com>
2021-12-01 02:04:24 +0530

    Create pytest.py

---
+++ pytest.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec  1 01:58:33 2021
+
+@author: kollisrivenkataphaniabhishek
+"""
+
+import pytest
+pytest.main()
+
+
+runfile('/Users/kollisrivenkataphaniabhishek/Downloads/mpcs programming/assignment-5/pytest.py', wdir='/Users/kollisrivenkataphaniabhishek/Downloads/mpcs programming/assignment-5')
+Reloaded modules: problem7, problem2, random_numbers, problem3_my, problem6, problem_1, test_problem_1, test_problem7, test_problem6, test_problem3, test_problem2
+============================= test session starts ==============================
+platform darwin -- Python 3.8.8, pytest-6.2.3, py-1.10.0, pluggy-0.13.1
+rootdir: /Users/kollisrivenkataphaniabhishek/Downloads/mpcs programming/assignment-5
+plugins: anyio-2.2.0
+collected 9 items
+
+test_problem2.py ...                                                     [ 33%]
+test_problem3.py .                                                       [ 44%]
+test_problem6.py .                                                       [ 55%]
+test_problem7.py .                                                       [ 66%]
+test_problem_1.py ...                                                    [100%]
+
+============================== 9 passed in 0.46s ===============================
